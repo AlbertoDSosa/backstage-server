4d52bc6077a732dd91c7b290c6ce7d799ccf988c
Alberto D.Sosa <albertodsosa@gmail.com>
2016-01-10 17:52:23 +0000

    get y post a users

--- api/users/index.js
+++ api/users/index.js
@@ -2,9 +2,8 @@
 var mongoose = require('mongoose');
 var express = require('express');
 
-
 var UserSchema = new mongoose.Schema({
-	userId: { type: Number },
+	userId: { type: Number, required: true, unique: true },
 	firstName: { type: String },
 	lastName: { type: String },
 
@@ -16,13 +15,49 @@ var model = mongoose.model('User', UserSchema);
 
 var router = express.Router();
 
-router.get('/users/', function (req, res) {
+router.get('/users', function (req, res) {
 	model.find({}, function (err, users) {
 		if(err){
-			return res.sendStatus(500).json(err)
+			return res.sendStatus(500).json(err);
 		}
 		res.json(users);
 	});
+});
+
+router.post('/user', function (req, res) {
+	var userId = 0, user;
+
+	var onSave = function (err) {
+		if(err){
+			res.sendStatus(500).json(err);
+		}
+		res.json(user);
+	}
+
+	model.find({}, function (err, users) {
+		if (users.length >= 1) {
+
+			var last = users.length - 1;
+			userId = users[last].userId;
+			user = new model();
+			user.userId = userId + 1;
+			user.firstName = req.body.firstName;
+			user.lastName = req.body.lastName;
+			user.userRole = req.body.userRole;
+			user.save(onSave);
+
+		} else {
+
+			user = new model();
+			user.userId = 1;
+			user.firstName = req.body.firstName;
+			user.lastName = req.body.lastName;
+			user.userRole = req.body.userRole;
+			user.save(onSave);
+		}
+	});
+
+
 });
 
 
--- index.js
+++ index.js
@@ -3,10 +3,21 @@ var	express = require('express');
 var app = express();
 var mongoose = require('mongoose');
 var api = require('./api');
+var bodyParser = require('body-parser');
 
 mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/backstage');
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.use(bodyParser.json());
+
 app.use('/', api);
+
+
 
 var server = app.listen(5000, function () {
   var host = server.address().address;
