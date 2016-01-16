4c0c78500805d131d2ec83a8590b60362f5499cd
Alberto D.Sosa <albertodsosa@gmail.com>
2016-01-16 20:29:35 +0000

    implementado DELETE y PUT

--- api/users/index.js
+++ api/users/index.js
@@ -32,35 +32,81 @@ router.post('/user', function (req, res) {
 			res.sendStatus(500).json(err);
 		}
 		res.json(user);
-	}
+	};
 
 	model.find({}, function (err, users) {
-		if (users.length >= 1) {
 
-			var last = users.length - 1;
-			userId = users[last].userId;
-			user = new model();
-			user.userId = userId + 1;
-			user.firstName = req.body.firstName;
-			user.lastName = req.body.lastName;
-			user.userRole = req.body.userRole;
-			user.save(onSave);
+			if (users.length >= 1) {
 
-		} else {
+				var last = users.length - 1;
+				userId = users[last].userId;
+				user = new model();
+				user.userId = userId + 1;
+				user.firstName = req.body.firstName;
+				user.lastName = req.body.lastName;
+				user.userRole = req.body.userRole;
+				user.save(onSave);
 
-			user = new model();
-			user.userId = 1;
-			user.firstName = req.body.firstName;
-			user.lastName = req.body.lastName;
-			user.userRole = req.body.userRole;
-			user.save(onSave);
-		}
+			} else {
+
+				user = new model();
+				user.userId = 1;
+				user.firstName = req.body.firstName;
+				user.lastName = req.body.lastName;
+				user.userRole = req.body.userRole;
+				user.save(onSave);
+			}
 	});
-
 
 });
 
+router.delete('/user/:userId', function (req, res) {
+	var userId = req.params.userId;
+
+	model.findOne({ userId: userId }, function (err, user) {
+		if(user){
+			user.remove(function (error, userRes) {
+				if(error){
+					res.sendStatus(500).json(error)
+				} else {
+					res.json(userRes);
+				}
+			});
+		} else {
+			res.sendStatus(500).json(err);
+		}
+	});
+
+});
+
+router.put('/user/:userId', function (req, res) {
+
+	model
+		.findOneAndUpdate(
+			{
+				userId: req.params.userId
+			},
+			{
+				$set: {
+					firstName: req.body.firstName,
+					lastName: req.body.lastName,
+					userRole: req.body.userRole
+				}
+			},
+
+			function (err, updateRes) {
+
+				if (err) {
+					res.sendStatus(500).json(err);
+				} else {
+					res.json(updateRes);
+				}
+
+			});
+
+});
 
 module.exports = {
-	router: router
+	router: router,
+	model: model
 }
--- index.js
+++ index.js
@@ -9,6 +9,8 @@ mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/backstage');
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header ('Access-Control-Allow-Credentials', true);
+  res.header ('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
